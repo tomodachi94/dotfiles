ef773a0455e919f0fa5839999b91a5a52aa56a91
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-10-22 14:14:19 -0700

    Fix UltiSnips dir

--- lua/vim-ultisnips.lua
+++ lua/vim-ultisnips.lua
@@ -1,6 +1,6 @@
 -- UltiSnips configuration
 
-vim.g.UltiSnipsSnippetDirectories=["UltiSnips", "snips"]
+vim.g.UltiSnipsSnippetDirectories = {"UltiSnips", "snips"}
 vim.g.UltiSnipsExpandTrigger = '<tab>'
 vim.g.UltiSnipsJumpForwardTrigger = '<tab>'
 vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'

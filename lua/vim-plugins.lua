ee1b85b51c5f647db6a22151a07acc2c3e7d5f04
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-10-22 17:31:05 -0700

    Add vim-just

--- lua/vim-plugins.lua
+++ lua/vim-plugins.lua
@@ -3,9 +3,9 @@ require "paq" {
     "tomodachi94/vim-mediawiki-editor"; -- MediaWiki support
     "chikamichi/mediawiki.vim";
     "sirver/UltiSnips"; -- snippets
+    {"dracula/vim", as="dracula"}; -- theme
+    "NoahTheDuke/vim-just";
     {"neoclide/coc.nvim", branch="release"}; -- autocompletion support
-    "quangnguyen30192/cmp-nvim-ultisnips";
-    {"dracula/vim", as="dracula"} -- theme
 }
 
 -- setup theming since this is the best spot for it

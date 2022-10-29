699aac2a535bd66d483a5f8c42dd369061697b3c
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-10-29 12:59:33 -0700

    Add behavior to esc in normal mode
    See https://nanotipsforvim.prose.sh/esc-in-normal-mode

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("source ~/.config/nvim/vimscript/no-autoload/source-vimscript.vim")
 
 require("plugins")
 require("syntax")
+require("keybinds")
 require("conf.mediawiki-editor")
 require("conf.mediawiki-editor-creds")
 require("conf.ultisnips")
---
+++ lua/keybinds.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")
+

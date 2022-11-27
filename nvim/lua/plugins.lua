d2dc519f383003ccec1f8336b0ac079e1335217b
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-27 10:16:38 -0800

    nvim: Organize plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,16 +1,23 @@
 require "paq" {
-    "savq/paq-nvim";
-    "aquach/vim-mediawiki-editor"; -- MediaWiki support
+    "savq/paq-nvim"; -- let Paq manage itself
+
+    {"dracula/vim", as="dracula"}; -- theme
+
+	-- syntax --
+	"vmchale/dhall-vim";
+	"gpanders/editorconfig.nvim";
     "chikamichi/mediawiki.vim";
-    "sirver/UltiSnips"; -- snippets
-    "vim-airline/vim-airline";
+    "NoahTheDuke/vim-just";
+
+	-- new features --
+	"sirver/UltiSnips"; -- snippets
+    "vim-airline/vim-airline"; -- fancy status bar
     "liuchengxu/vim-which-key";
     "nvim-tree/nvim-web-devicons";
+	"aquach/vim-mediawiki-editor"; -- edit MediaWiki inside of neovim
     "nvim-lua/plenary.nvim";
     "nvim-telescope/telescope.nvim";
-    {"dracula/vim", as="dracula"}; -- theme
-    "NoahTheDuke/vim-just";
-    {"neoclide/coc.nvim", branch="release"}; -- autocompletion support
+    {"neoclide/coc.nvim", branch="release"}; -- autocompletion support, we should switch to neovim/lspconfig.nvim
 }
 
 -- setup theming since this is the best spot for it

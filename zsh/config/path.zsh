7bfdff0a9e503a5351ac98fea08bb47a43a6859f
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-30 06:59:44 -0800

    zsh: Use $HOME when determining path

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -1,22 +1,22 @@
 echo "Setting up PATH..."
 
-PATH="~/bin:~/.local/bin:$PATH"
+PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 
 if (( $+commands[sdk] )) then
-	export SDKMAN_DIR="~/.sdkman"
-	source ~/.sdkman/bin/sdkman-init.sh
+	export SDKMAN_DIR="$HOME/.sdkman"
+	source $HOME/.sdkman/bin/sdkman-init.sh
 fi
 
 if (( $+commands[rvm] )) then
-	export PATH="~/.rvm/bin:$PATH"
+	export PATH="$HOME/.rvm/bin:$PATH"
 fi
 
 if (( $+commands[brew] )) then
 	brew shellenv | eval
-	PATH="~/.nodebrew/current/bin:$PATH"
+	PATH="$HOME/.nodebrew/current/bin:$PATH"
 fi
 
 if (( $+commands[cargo] )) then
-	PATH="~/.cargo/bin:$PATH"
+	PATH="$HOME/.cargo/bin:$PATH"
 fi
 

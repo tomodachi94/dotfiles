928f794bc7f5980538b2b9321a9b6d7ae52449e4
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-21 11:05:17 -0800

    zsh: Add nnn's `quitcd` script
    Realized that zplug can indeed manage this.
    
    Documentation: https://github.com/jarun/nnn/wiki/Basic-use-cases#configure-cd-on-quit

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -7,6 +7,9 @@ zplug "zsh-users/zsh-syntax-highlighting"
 
 zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
 
+# Play nice with the nnn file manager
+zplug "jarun/nnn", use:"misc/quitcd/quitcd.bash_zsh", if:"which nnn"
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf "Install plugins with zplug? [y/N]: "

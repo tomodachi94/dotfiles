70f67b6352793cefa6e49bba500b6dd6024368b1
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-20 20:38:23 -0800

    zsh: Remove command-not-found
    Slows down things when a command is mistyped.

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 source ~/.zplug/init.zsh
 
-zplug "plugins/command-not-found", from:oh-my-zsh
-
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting"

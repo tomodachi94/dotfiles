e8237c2b388bd71a9d033a05dd334384f9aeb3ad
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-26 18:28:42 -0800

    zsh: Add nix-shell compat

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -14,6 +14,9 @@ zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
 # Play nice with the nnn file manager
 zplug "jarun/nnn", use:"misc/quitcd/quitcd.bash_zsh", if:"which nnn"
 
+# Play nice with Nix/NixOS
+zplug "chisui/zsh-nix-shell", if:"which nix-shell"
+
 # Completions
 # Only load these if the executable is present
 zplug "casey/just", use:"completions/just.zsh", if:"which just"

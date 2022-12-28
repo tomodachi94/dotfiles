89c4b1f3e2635226b06cf2c9f6a46a0b400bc9fa
Tomo <68489118+tomodachi94@users.noreply.github.com>
2022-12-28 08:41:58 +0000

    nvim: Move `init.lua` into module

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,1 @@
--- loads old vimscript things
-vim.cmd("source ~/.config/nvim/vimscript/no-autoload/source-vimscript.vim")
-
-require("tomodachi94.setup-plugins")
-require("tomodachi94.options")
-require("tomodachi94.keybinds")
-require("tomodachi94.filetype.mcmeta")
-require("tomodachi94.filetype.gemrc")
-require("tomodachi94.filetype.rvmrc")
+require("tomodachi94")
---
+++ nvim/lua/tomodachi94/init.lua
@@ -0,0 +1,12 @@
+-- This code is in the lua/tomodachi94 directory so that the code remains self-contained.
+-- The root init.lua should require() this code.
+
+-- loads old vimscript things
+vim.cmd("source ~/.config/nvim/vimscript/no-autoload/source-vimscript.vim")
+
+require("tomodachi94.setup-plugins")
+require("tomodachi94.options")
+require("tomodachi94.keybinds")
+require("tomodachi94.filetype.mcmeta")
+require("tomodachi94.filetype.gemrc")
+require("tomodachi94.filetype.rvmrc")

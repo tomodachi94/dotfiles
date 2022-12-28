d9c2c50a13d24bacf67f43986461d40f81bc76b4
Tomo <68489118+tomodachi94@users.noreply.github.com>
2022-12-28 08:57:57 +0000

    nvim: Move some VimL keybinds to Lua

--- nvim/lua/tomodachi94/keybinds.lua
+++ nvim/lua/tomodachi94/keybinds.lua
@@ -1,2 +1,7 @@
 vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")
 
+-- Convinience functions for yanking/putting
+vim.keymap.set("n", "<Leader>y", "*y")
+vim.keymap.set("n", "<Leader>p", "*p")
+vim.keymap.set("n", "<Leader>Y", "+y")
+vim.keymap.set("n", "<Leader>P", "+p")
--- nvim/vimscript/keybinds.vim
+++
@@ -1,4 +0,0 @@
-noremap <Leader>y "*y
-noremap <Leader>p "*p
-noremap <Leader>Y "+y
-noremap <Leader>P "+p

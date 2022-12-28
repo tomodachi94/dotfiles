52056f3d5b9462c42818f707a167c030907d92de
Tomo <68489118+tomodachi94@users.noreply.github.com>
2022-12-28 08:31:39 +0000

    nvim: Remove `mediawiki-editor-creds.lua` feature

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -2,10 +2,6 @@
 
 git submodule update --init --recursive --depth 1
 
-{{#if dotter.packages.nvim}}
-touch {{dotter.current_dir}}/nvim/lua/conf/mediawiki-editor-creds.lua # nvim throws a warning if this file doesn't exist
-{{/if}}
-
 {{#if dotter.package.gnome-terminal}}
 cat {{dotter.current_dir}}/gnome-terminal/gnome-terminal.dconf | dconf load /org/gnome/terminal
 {{/if}}
--- nvim/lua/tomodachi94/plugins/mediawiki-editor.lua
+++ nvim/lua/tomodachi94/plugins/mediawiki-editor.lua
@@ -5,8 +5,6 @@ function M.config()
 	vim.g.mediawiki_editor_url = "ftb.fandom.com"
 	vim.g.mediawiki_editor_path = "/"
 	vim.g.mediawiki_editor_uri_scheme = "https"
-	require("tomodachi94.plugins.mediawiki-editor-creds")
 end
---- sensitive plugin configuration is loaded in /init.lua ---
 
 return {M}

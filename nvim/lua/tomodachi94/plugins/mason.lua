0ef12df3fdf78e598fac31d9c7f78e9ed63d40df
Tomo <68489118+tomodachi94@users.noreply.github.com>
2022-12-28 18:18:14 +0000

    nvim: Install Mason.nvim

--- nvim/lua/tomodachi94/plugins/mason.lua
+++ nvim/lua/tomodachi94/plugins/mason.lua
@@ -1,4 +1,16 @@
-local M = {"/williamboman/mason.nvim", config = true, enabled = false}
+local M = {"williamboman/mason.nvim", config = true}
+
+function M.config()
+	require("mason").setup{
+		ui = {
+				icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗"
+				}
+			}
+	}
+end
 
 -- Disabled for now
 return {M}

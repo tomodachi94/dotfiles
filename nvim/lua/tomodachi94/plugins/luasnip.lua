a584f1f69189deb656592ea91fd7463793ac78b5
Tomo <68489118+tomodachi94@users.noreply.github.com>
2022-12-28 18:14:33 +0000

    nvim: Setup LuaSnip
    Disabled for now.

---
+++ nvim/lua/tomodachi94/plugins/luasnip.lua
@@ -0,0 +1,22 @@
+-- LuaSnip configuration
+
+local M = {"L3MON4D3/LuaSnip", enabled = false}
+
+-- Source: https://www.reddit.com/r/neovim/comments/zrcrv1/luasnip_tab_as_expandjump_trigger/j12xw3x/#c
+local function luasnip_tab()
+	local luasnip = require("luasnip")
+	if luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	else
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false) -- Feed <Tab> back to Neovim
+	end
+end
+
+function M.config()
+	require("luasnip.loaders.from_snipmate").lazy_load({paths = "./snippets"})
+	vim.keymap.set({"i", "s"}, "<Tab>", luasnip_tab, {silent = true})
+
+
+end
+
+return {M}

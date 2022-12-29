52b3f60c58efe74861c5d2fcb19539d4d1edcc3e
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-28 16:54:45 -0800

    nvim: Cleanup `cmp.nvim` config
    - Fix the configuration, it was broken before.
    - Use StyLua to unify formatting.

--- nvim/lua/tomodachi94/plugins/cmp.lua
+++ nvim/lua/tomodachi94/plugins/cmp.lua
@@ -14,7 +14,7 @@ function M.config()
 		snippet = {
 			-- REQUIRED - you must specify a snippet engine
 			expand = function(args)
-				vim.fn["UltiSnips#Anon"](args.body) -- For `luasnip` users.
+				vim.fn["UltiSnips#Anon"](args.body) -- For `UltiSnips` users.
 			end,
 		},
 		window = {
@@ -22,52 +22,53 @@ function M.config()
 			-- documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
-			['<C-b>'] = cmp.mapping.scroll_docs(-4),
-			['<C-f>'] = cmp.mapping.scroll_docs(4),
-			['<C-Space>'] = cmp.mapping.complete(),
-			['<C-e>'] = cmp.mapping.abort(),
-			['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	formatting = {
-		format = lspkind.cmp_format(),
-	},
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		}),
+		formatting = {
+			format = lspkind.cmp_format(),
+		},
 		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'ultisnips' }, -- For luasnip users.
-		}, {
-			{ name = 'buffer' },
-		})
+			{ name = "nvim_lsp" },
+			{ name = "ultisnips" }, -- For luasnip users.
+		}),
+		{
+			{ name = "buffer" },
+		},
 	})
-
 	-- Set configuration for specific filetype.
-	cmp.setup.filetype('gitcommit', {
+	cmp.setup.filetype("lua", {
 		sources = cmp.config.sources({
-			{ name = 'buffer' },
-		})
+			{ name = "nvim_lsp" },
+			{ name = "ultisnips" },
+		}),
 	})
 
 	-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-	cmp.setup.cmdline({ '/', '?' }, {
+	cmp.setup.cmdline({ "/", "?" }, {
 		mapping = cmp.mapping.preset.cmdline(),
 		sources = {
-			{ name = 'buffer' }
-		}
+			{ name = "buffer" },
+		},
 	})
 
 	-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-	cmp.setup.cmdline(':', {
+	cmp.setup.cmdline(":", {
 		mapping = cmp.mapping.preset.cmdline(),
 		sources = cmp.config.sources({
-			{ name = 'path' }
+			{ name = "path" },
 		}, {
-			{ name = 'cmdline' }
-		})
+			{ name = "cmdline" },
+		}),
 	})
 
 	-- Set up lspconfig.
-	local capabilities = require('cmp_nvim_lsp').default_capabilities()
-	require('lspconfig')['sumneko_lua'].setup {
-		capabilities = capabilities
-	}
-}
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	require("lspconfig")["sumneko_lua"].setup({
+		capabilities = capabilities,
+	})
 end
-return {M, N, O, P, Q, R}
+return { M, N, O, P, Q, R }

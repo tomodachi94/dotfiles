64532908202c456c8babadb91894afb8eff4cd13
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-03 20:50:24 -0800

    nixpkgs(home): Add notes app to `packages`

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -30,6 +30,10 @@
     # Chat
     pkgs.discord
     pkgs.element-desktop
+
+    # Notes
+    pkgs.obsidian
+    pkgs.jot
   ];
 
   # This value determines the Home Manager release that your
@@ -126,4 +130,35 @@
       style = "compact";
     };
   };
+
+/* TODO: Fix colors.palette
+  programs.gnome-terminal = {
+    enable = true;
+    profile.b1dcc9dd-5262-4d8d-a863-c897e6d979b9 = {
+      loginShell = true;
+      customCommand = null;
+      font = "BlexMono Nerd Font weight=453 12";
+      visibleName = "tomodachi94";
+      colors = {
+        palette = [
+          ''rgb(46,52,54)''
+          ''rgb(204,0,0)''
+          ''rgb(78,154,6)''
+          ''rgb(196,160,0)''
+          ''rgb(52,101,164)''
+          ''rgb(117,80,123)''
+          ''rgb(6,152,154)''
+          ''rgb(211,215,207)''
+          ''rgb(85,87,83)''
+          ''rgb(239,41,41)''
+          ''rgb(138,226,52)''
+          ''rgb(252,233,79)''
+          ''rgb(114,159,207)''
+          ''rgb(173,127,168)''
+          ''rgb(52,226,226)''
+          ''rgb(238,238,236)''
+        ];
+      };
+    };
+  }; */
 }

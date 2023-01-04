460bd27202270bdc32d38c7f54022407d9e0bc76
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-03 20:59:41 -0800

    nvim(plugins): Add vim-fugitive
    Likely the best Git plugin for (Neo)vim.

---
+++ nvim/lua/tomodachi94/plugins/fugitive.lua
@@ -0,0 +1,21 @@
+local M = {
+	"tpope/vim-fugitive",
+	lazy = true,
+	cmd = {
+		"Git",
+		"G",
+		"Gedit",
+		"Gdiffsplit",
+		"Gread",
+		"Gwrite",
+		"Ggrep",
+		"Glgrep",
+		"GMove",
+		"GRename",
+		"GDelete",
+		"GBrowse"
+	},
+	config = false,
+}
+
+return { M }

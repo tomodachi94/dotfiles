2086b7f8dd44a82e19bef520c81254368d4f92f8
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-14 12:39:52 -0800

    nvim: Set leader to space

--- nvim/lua/tomodachi94/keybinds.lua
+++ nvim/lua/tomodachi94/keybinds.lua
@@ -1,7 +1,11 @@
-vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")
+vim.g.mapleader = ' '
+
+vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>") -- Clear highlighting and buffer
 
 -- Convinience functions for yanking/putting
 vim.keymap.set("n", "<Leader>y", "*y")
 vim.keymap.set("n", "<Leader>p", "*p")
 vim.keymap.set("n", "<Leader>Y", "+y")
 vim.keymap.set("n", "<Leader>P", "+p")
+
+vim.keymap.set("n", "<Leader>fd", vim.cmd.NERDTreeToggle)

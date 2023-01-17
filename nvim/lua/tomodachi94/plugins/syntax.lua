1fe20c89b0e33a35b63d3f8817523bb2075b387e
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-16 21:46:29 -0800

    nvim: Use `tree-sitter` instead of `vim-polyglot`

--- nvim/lua/tomodachi94/plugins/syntax.lua
+++ nvim/lua/tomodachi94/plugins/syntax.lua
@@ -2,6 +2,11 @@
 -- Plugins whose primary function is to do something else should go into a new file
 -- Otherwise, this is the place.
 
-local M = { "sheerun/vim-polyglot" }
+-- local M = { "sheerun/vim-polyglot" }
+
+local M = {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate"
+}
 
 return { M }

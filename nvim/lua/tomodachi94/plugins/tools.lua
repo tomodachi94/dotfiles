b05f4d82190e2133651779ca9d33fd14a18c5d3f
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-03-04 10:46:51 -0800

    nvim(telescope): Enable LSP-related pickers

--- nvim/lua/tomodachi94/plugins/tools.lua
+++ nvim/lua/tomodachi94/plugins/tools.lua
@@ -47,11 +47,13 @@ local O = {
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
 	lazy = true,
 	cmd = "Telescope",
-	keys = {"<Leader>ff", "<Leader>fg"},
+	keys = {"<Leader>ff", "<Leader>fg", "<Leader>fr", "<Leader>fs"},
 	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+		vim.keymap.set('n', '<leader>fs', builtin.lsp_symbols, {})
 	end
 }
 
@@ -63,4 +65,12 @@ end
 
 local P = { "liuchengxu/vim-which-key", lazy = true, cmd = "WhichKey" }
 
-return { M, N, O, P }
+local Q = { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons", "https://github.com/folke/lsp-colors.nvim" }, cmd = { "Trouble", "TroubleClose", "TroubleToggle", "TroubleRefresh" } }
+
+function Q.config()
+	require("trouble").setup {
+		
+	}
+end
+
+return { M, N, O, P, Q }

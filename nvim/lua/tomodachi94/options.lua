78a5f100ea364bb6eec3c9ee67850603a57c9158
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-03-03 16:36:58 -0800

    nvim: Bind arrow keys to `gk`/`gj`
    This maps the arrow keys to commands that would
    go up/down a line visually, instead of where the
    linebreaks are.
    
    See:
    https://discord.com/channels/686053708261228577/1056113012999720980/1081365704919490590

--- nvim/lua/tomodachi94/options.lua
+++ nvim/lua/tomodachi94/options.lua
@@ -3,6 +3,11 @@ vim.opt.shiftwidth = 4
 
 vim.opt.termguicolors = true
 
+vim.cmd("xnoremap j gj")
+vim.cmd("xnoremap k gk")
+vim.cmd("xnoremap <Down> gj")
+vim.cmd("xnoremap <Up> gk")
+
 -- enables per-filetype indentation settings
 if vim.fn.has("autocmd") == 1 then
 	vim.cmd("filetype indent plugin on")

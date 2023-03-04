7cf99fdff995acf717e6fef554ea056bf8798cbb
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-03-04 10:47:50 -0800

    nvim(lsp): Enable null-ls

--- nvim/lua/tomodachi94/plugins/lsp.lua
+++ nvim/lua/tomodachi94/plugins/lsp.lua
@@ -22,6 +22,26 @@ function M.config()
 	require("lspconfig")["rnix"].setup {
 		capabilities = capabilities,
 	}
+
+local N = { "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } }
+
+function N.config()
+	local null_ls = require("null-ls")
+	local sources = null_ls.builtins
+
+	sources.code_actions.shellcheck.filetypes = { "sh", "bash", "zsh" }
+
+	null_ls.setup {
+		sources = {
+			sources.formatting.stylua,
+			sources.diagnostics.selene,
+			sources.code_actions.proselint,
+			sources.code_actions.shellcheck,
+			sources.completion.spell,
+			sources.diagnostics.ruff,
+			sources.diagnostics.mypy,
+		}
+	}
 end
 
 return { M }

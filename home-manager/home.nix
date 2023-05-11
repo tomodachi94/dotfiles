6fd140790f2fa45e36a534104a91de4eb8f8ba92
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-05-10 18:04:33 -0700

    home-manager: Add Cachix repositories

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -76,6 +76,8 @@ in
     package = lib.mkDefault pkgs.nix;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI=" ];
     };
   };
   

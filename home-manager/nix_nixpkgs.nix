3653f1d1eae6c20997d62d8f5f68f90024d06096
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-06-22 10:15:06 -0700

    nix: auto-optimize store

--- home-manager/nix_nixpkgs.nix
+++ home-manager/nix_nixpkgs.nix
@@ -4,9 +4,13 @@
   nixpkgs = {
     config = {
       allowUnfreePredicate = _: true;
+      permittedInsecurePackages = [
+        "openssl-1.1.1t"
+      ];
     };
     overlays = [
         nur.overlay
+        (import ../nixpkgs/overlays/nerdfonts.nix)
     ];
   };
 
@@ -14,8 +18,9 @@
     package = lib.mkDefault pkgs.nix;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" ];
+      substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI=" ];
+      auto-optimise-store = true;
     };
   };
 }

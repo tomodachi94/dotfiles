79d4d5188f2d1047d52d71a333c162fd64011093
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-06-24 18:03:30 -0700

    nix: enable extra platforms

--- home-manager/nix_nixpkgs.nix
+++ home-manager/nix_nixpkgs.nix
@@ -1,4 +1,4 @@
-{ environment, config, pkgs, lib, nur, ... }:
+{ environment, config, pkgs, lib, nur, nixpkgs-ruby, ... }:
 {
   # Let Home Manager manage Nix and Nixpkgs for us
   nixpkgs = {
@@ -10,6 +10,7 @@
     };
     overlays = [
         nur.overlay
+        # nixpkgs-ruby.overlays.default
         (import ../nixpkgs/overlays/nerdfonts.nix)
     ];
   };
@@ -21,6 +22,7 @@
       substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI=" ];
       auto-optimise-store = true;
+      extra-platforms = "aarch64-linux";
     };
   };
 }

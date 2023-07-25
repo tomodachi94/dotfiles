d749fe1544c61fde462806818bea80b9ab003adf
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-07-25 14:39:06 -0700

    nvim(support_mediawiki_editing): Fetch credentials from keyring

--- nvim/lua/tomodachi94/plugins/support_mediawiki_editing.lua
+++ nvim/lua/tomodachi94/plugins/support_mediawiki_editing.lua
@@ -1,10 +1,14 @@
 local M = { "aquach/vim-mediawiki-editor", lazy = true, cmd = { "MWRead", "MWWrite", "MWDiff", "MWBrowse" } }
 
+local keyring = require("keyring")
+
 --- MediaWiki plugin configuration ---
 function M.config()
 	vim.g.mediawiki_editor_url = "ftb.fandom.com"
 	vim.g.mediawiki_editor_path = "/"
 	vim.g.mediawiki_editor_uri_scheme = "https"
+	vim.g.mediawiki_editor_username = "Tomodachi94@nvim"
+	vim.g.mediawiki_editor_password = keyring.get(vim.g.mediawiki_editor_url, vim.g.mediawiki_editor_username)
 end
 
 return { M }

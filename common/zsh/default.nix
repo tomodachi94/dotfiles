7309ef9a5660e4f7b213f699fad16b54a6efae7b
Tomo <68489118+tomodachi94@users.noreply.github.com>
2024-03-06 12:50:53 -0800

    zsh

--- common/zsh/default.nix
+++ common/zsh/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 # Let's start migrating Zsh here
 {
+  imports = [ ./aliases.nix ./plugins.nix ];
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
@@ -12,36 +13,5 @@
       ignoreSpace = true;
       ignoreDups = true;
     };
-
-    shellAliases = {
-      ls = "eza";
-      tree = "eza --tree";
-      lsdot = "ls -ad .?* 2>/dev/null'"; # Source: https://github.com/Nekohime/dotfiles/blob/main/.aliases
-
-      # Common typos
-      sl = "ls";
-    };
-
-    plugins = [
-      {
-        name = "nix-zsh-completions";
-        src = "${pkgs.nix-zsh-completions}/share/zsh/site-functions";
-      }
-      /* {
-                   name = "nix-shell";
-                   src = "${pkgs.zsh-nix-shell}/share/zsh/site-functions";
-                   } */
-      {
-        name = "nnn-quitcd";
-        file = "misc/quitcd/quitcd.bash_sh_zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "jarun";
-          repo = "nnn";
-          rev = "efd5bc9db100d0489cfb3d982a69b04dacaff852";
-          sha256 = "sha256-A0XxC8/DNb2GuIxPVUiWNsfaq+UysVRYHG8fl/Iyaaw=";
-        };
-      }
-
-    ];
   };
 }
---
+++ common/zsh/plugins.nix
@@ -0,0 +1,23 @@
+{
+  plugins = [
+  {
+    name = "nix-zsh-completions";
+    src = "${pkgs.nix-zsh-completions}/share/zsh/site-functions";
+  }
+  {
+     name = "nix-shell";
+     src = "${pkgs.zsh-nix-shell}/share/zsh/site-functions";
+  }
+  {
+    name = "nnn-quitcd";
+    file = "misc/quitcd/quitcd.bash_sh_zsh";
+    src = pkgs.fetchFromGitHub {
+      owner = "jarun";
+      repo = "nnn";
+      rev = "efd5bc9db100d0489cfb3d982a69b04dacaff852";
+      sha256 = "sha256-A0XxC8/DNb2GuIxPVUiWNsfaq+UysVRYHG8fl/Iyaaw=";
+    };
+  }
+
+  ];
+}

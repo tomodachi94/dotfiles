4c03d977bcb3eb369691982e5cb6885b4a88292e
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-17 14:53:18 -0700

    zsh: fix lsdot alias

--- home/common/zsh/aliases.nix
+++ home/common/zsh/aliases.nix
@@ -2,7 +2,7 @@
   programs.zsh.shellAliases = {
     ls = "eza";
     tree = "eza --tree";
-    lsdot = "ls -ad .?* 2>/dev/null'"; # Source: https://github.com/Nekohime/dotfiles/blob/main/.aliases
+    lsdot = "ls -ad .?* 2>/dev/null"; # Source: https://github.com/Nekohime/dotfiles/blob/main/.aliases
 
 # Common typos
       sl = "ls";

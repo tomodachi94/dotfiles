e934a13f4e7e07c5c200da6ddd0d5ae10e378a55
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-16 17:18:56 -0700

    swap gdm with lightdm

--- nixos/window-manager/default.nix
+++ nixos/window-manager/default.nix
@@ -4,8 +4,17 @@
     # Enable the X11 windowing system.
     enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    displayManager.gdm.enable = true;
+    # Enable the LightDM desktop manager.
+	displayManager.defaultSession = "none+i3";
+	displayManager.lightdm = {
+	  enable = true;
+	  # TODO: change wallpaper
+	  greeters.slick = {
+        enable = true;
+		extraConfig = ''user-background = false'';
+	  };
+	};
+    # Enable the i3 window manager.
     windowManager.i3.enable = true;
   };
 }

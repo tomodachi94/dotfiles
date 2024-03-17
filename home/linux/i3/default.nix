310f8a2407432409d1248a2a04d121d1be79753a
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-17 13:55:37 -0700

    split i3 config

--- home/linux/i3/default.nix
+++ home/linux/i3/default.nix
@@ -1,9 +1,15 @@
 { pkgs, xdg, ... }:
 {
   xdg.configFile."i3/config".text = ''
-    set $wallpaper_path "${./wallpaper.png}"
-    ${builtins.readFile ./config}
+# avoid polluting $HOME with symlinks by setting some paths at the config build stage
+set $wallpaper_path "${./wallpaper.png}"
+include ${./all.config}
+include ${./background.config}
+include ${./programs.config}
+include ${./keybinds.config}
+include ${./bar.config}
   '';
+  home.file.".background-image".source = ./wallpaper.png;
   # For cleanliness, this contains any packages that are exclusively used in this configuration.
   home.packages = with pkgs; [
   	xss-lock

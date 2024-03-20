036c3bc87eafd0fa86cb25011082bee0310b1447
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-19 19:24:30 -0700

    zsh: fix compinit warning
    https://discourse.nixos.org/t/zsh-compinit-warning-on-every-shell-session/22735/7

--- home/common/zsh/default.nix
+++ home/common/zsh/default.nix
@@ -8,6 +8,7 @@
 	  enable = true;
 	};
     enableCompletion = true;
+	completionInit = "autoload -U compinit && compinit -i";
     syntaxHighlighting.enable = true;
     autocd = true;
     initExtra = ". ~/.zsh/init.zsh"; # TODO: This has things that haven't been migrated to Nix; we should finish that

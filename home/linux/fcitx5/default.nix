c8780e4fab77e7aa71d1c4f3fad72a1c199c58c2
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-23 22:17:05 -0700

    fcitx5: add addons

--- home/linux/fcitx5/default.nix
+++ home/linux/fcitx5/default.nix
@@ -1,5 +1,10 @@
+{ pkgs, ... }:
 {
   i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-mozc
+    fcitx5-gtk
+  ];
   xdg.configFile."fcitx5/profile".source = ./profile;
   xdg.configFile."fcitx5/config".source = ./config;
   xdg.configFile."fcitx5/conf/mozc.conf".source = ./mozc.conf;

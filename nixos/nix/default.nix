9f2929a54e6a49830a6e1890250109dfad979e24
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-27 22:32:53 -0700

    nix: add substituters

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -6,6 +6,14 @@
 		"flakes"
 	  ];
 	  auto-optimise-store = true;
+      trusted-substituters = [
+        "https://tomodachi94.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+	    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+		"tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI="
+	  ];
 	};
 	gc = {
 	  automatic = true;

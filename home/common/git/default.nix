ca048840e4bfa47c2f4a634356acbfdd993f4e81
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-28 00:05:33 -0700

    git: cleanup config

--- home/common/git/default.nix
+++ home/common/git/default.nix
@@ -3,9 +3,6 @@
   programs.git = {
     enable = true;
     includes = [
-      /* {
-        path = ../../git/config;
-      } */
       {
         path = "~/.git-identities";
       }
@@ -13,11 +10,8 @@
     aliases = {
       s = "status";
       unadd = "reset HEAD --";
-      visual = "!gitk";
       c = "commit";
       last = "log -1 HEAD";
-      cif = "commit --fixup";
-      ri = "rebase -i --autosquash";
       xdg-open = "!${pkgs.git}/bin/git remote show origin | ${pkgs.gnugrep}/bin/grep https:// | ${pkgs.gnused}/bin/sed 's/Fetch URL: //g' | ${pkgs.gnused}/bin/sed 's/Push  URL: //g' | ${pkgs.coreutils}/bin/uniq | ${pkgs.findutils}/bin/xargs -n 1 ${pkgs.xdg-utils}/bin/xdg-open"; # Open Git remote 'origin' in default web browser"
     };
 
@@ -35,30 +29,38 @@
         algorithm = "histogram";
         tool = "${pkgs.lib.getExe pkgs.neovim} -d";
 	  };
+	  # Guesses what I mean when I make a mistake
       help = {
-        autocorrect = 20;
+        autocorrect = "prompt";
       };
+	  # Reduce mistakes
       url = {
         "https://github.com/" = {
           insteadOf = "git://github.com";
         };
       };
+	  # Show full diff when running `git commit`
 	  commit = {
         verbose = true;
 	  };
+	  # Push to branch origin/whatever_my_current_branch_is if no upstream branch is set
 	  push = {
         default = "current";
 	  };
+	  # Always `git pull --rebase`.
 	  pull = {
         rebase = true;
 	  };
+	  # Better diff style
 	  merge = {
         conflictstyle = "zdiff3";
 	  };
+	  # Helpful for rebases
 	  rebase = {
         autosquash = true;
 		autostash = true;
 	  };
+	  # Run some validation on Git objects to prevent weirdness from occuring
 	  transfer = {
 	    fsckobjects = true;
       };
@@ -68,6 +70,7 @@
       receive = {
 	    fsckObjects = true;
 	  };
+	  # Cache git logins
       credential = {
         helper = [ "cache --timeout 14440" ];
       };

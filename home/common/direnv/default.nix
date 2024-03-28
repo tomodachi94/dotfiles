0ce53e320d6bc77bad4aa0d118a0e84a837f55f3
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-27 20:06:58 -0700

    direnv: add some config

--- home/common/direnv/default.nix
+++ home/common/direnv/default.nix
@@ -4,5 +4,11 @@
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
+	config = {
+      global = {
+        hide_env_diff = true;
+		warn_timeout = "1m";
+	  };
+	};
   };
 }

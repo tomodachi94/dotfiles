79a60453882c2edfae98186fc622484f30f4e396
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-03-28 19:08:18 -0700

    remove duplicated and/or unused packages
    The duplicated packages happened because I added modules but didn't
    remove the separate addition to `home.packages`.
    
    Wonder if there's a way I can lint for this automatically...

--- home/common/shell-standalone-packages/default.nix
+++ home/common/shell-standalone-packages/default.nix
@@ -2,17 +2,13 @@
 {
   home.packages = [
     pkgs.coreutils # Duh
-    pkgs.zsh # Shell
     pkgs.neovim # Text editor
-    pkgs.eza # Better ls
     pkgs.fd # Find stuff
     pkgs.ripgrep # Better grep
     pkgs.renameutils # Do repetitive stuff faster
     pkgs.github-cli
     # pkgs.nnn # File manager
     pkgs.starship # Shell prompt
-    pkgs.atuin # Better shell history
-    pkgs.direnv
     # pkgs.nur.repos.ftbwiki.ftb-rs
   ];
 }
--- home/linux/desktop-standalone-packages/default.nix
+++ home/linux/desktop-standalone-packages/default.nix
@@ -3,10 +3,6 @@
   home.packages = [
     pkgs.lxqt.qps
     pkgs.vscodium-fhs # GUI IDE
-    pkgs.kazam
-    pkgs.blueman
-    pkgs.eww
-    pkgs.gnome.gnome-terminal # Terminal
     pkgs.librewolf # Web browser
     pkgs.zotero
   ];

dce67b16ee1d48aec40f8729dbb8479f863b21d2
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-04-05 21:49:34 -0700

    zsh: stop sourcing ~/.zsh/init.zsh

--- home/common/zsh/default.nix
+++ home/common/zsh/default.nix
@@ -11,9 +11,6 @@
     completionInit = "autoload -U compinit && compinit -i";
     syntaxHighlighting.enable = true;
     autocd = true;
-    initExtra = ". ~/.zsh/init.zsh"; # TODO: This has things that haven't been migrated to Nix; we should finish that
-    initExtraBeforeCompInit = ''
-    '';
     history = {
       ignoreSpace = true;
       ignoreDups = true;

87fe2c5c47fa7c91e48a155ec2132febd01ed826
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-04-20 16:49:58 -0700

    home/common/bat: switch to HM module

--- home/common/bat/default.nix
+++ home/common/bat/default.nix
@@ -1,3 +1,10 @@
 {
-  xdg.configFile."bat".source = ./.;
+  programs.bat = {
+    enable = true;
+    # Source: https://github.com/chrisgrieser/.config/blob/main/bat/config#L13-L14
+    # show percentage indicator
+    config = {
+      pager = "less -RF --long-prompt +Gg";
+    };
+  };
 }

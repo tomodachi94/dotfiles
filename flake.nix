de4f561f77812ad873b8637f3894b1ff773b2d2f
Tomo <68489118+tomodachi94@users.noreply.github.com>
2024-04-24 12:34:43 -0700

    flake.nix: add nixConfig attribute

--- flake.nix
+++ flake.nix
@@ -90,4 +90,15 @@
 
       devShells = tomolib.forAllSystems (pkgs: import ./lib/shells.nix { inherit pkgs home-manager; });
     };
+
+  nixConfig = {
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://tomodachi94.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI="
+    ];
+  };
 }

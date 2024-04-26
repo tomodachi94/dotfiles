8201b5dd972a0b71c205a3cd80ae39f0a5c5f11d
Tomo <68489118+tomodachi94@users.noreply.github.com>
2024-04-26 10:26:10 -0700

    pkgs/awesome-lint: remove from pkgs/default.nix
    https://github.com/tomodachi94/dotfiles/issues/25

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@ let
   cp = pkgs.callPackage;
 in
 {
-  awesome-lint = cp ./all/awesome-lint { };
+  # awesome-lint = cp ./all/awesome-lint { };
   developercolorpicker = cp ./darwin/developercolorpicker.nix { };
   hugo-bearblog = cp ./all/hugo-bearblog.nix { };
   spyglass = cp ./all/spyglass { };

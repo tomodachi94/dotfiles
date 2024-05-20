04c9d77b510c3c2bc5bc5d93b2d740f6f68fd28c
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-05-19 18:39:26 -0700

    lib/checks.nix: enable linting

--- lib/checks.nix
+++ lib/checks.nix
@@ -3,10 +3,16 @@
   pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
     src = ./.;
     hooks = {
-      ci = {
+      ci-check-formatting = {
         enable = true;
         name = "just ci: check formatting";
         entry = "${pkgs.just}/bin/just check-formatting";
+        pass_filenames = false;
+      };
+      ci-linting = {
+        enable = true;
+        name = "just ci: lint";
+        entry = "${pkgs.just}/bin/just lint";
         pass_filenames = false;
       };
     };

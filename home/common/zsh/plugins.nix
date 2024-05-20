029876339f51024dd83b93aeb17de0276488589d
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2024-05-20 16:49:11 -0700

    zsh/plugins: get quitcd from package directly
    This removes the need to download nnn's source, saving us a few seconds.
    
    I also don't need to periodically bump that fetcher hash, since it
    happens every time nixpkgs gets bumped.

--- home/common/zsh/plugins.nix
+++ home/common/zsh/plugins.nix
@@ -11,13 +11,8 @@
     }
     {
       name = "nnn-quitcd";
-      file = "misc/quitcd/quitcd.bash_sh_zsh";
-      src = pkgs.fetchFromGitHub {
-        owner = "jarun";
-        repo = "nnn";
-        rev = "efd5bc9db100d0489cfb3d982a69b04dacaff852";
-        sha256 = "sha256-A0XxC8/DNb2GuIxPVUiWNsfaq+UysVRYHG8fl/Iyaaw=";
-      };
+      file = "share/quitcd/quitcd.bash_sh_zsh";
+      src = pkgs.nnn;
     }
     {
       name = "craftos-select";

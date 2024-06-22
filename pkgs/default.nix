f8ab426b502a040b70907390f924819bc5047e09
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-22 15:11:07 -0700

    pkgs/awesome-lint: drop

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,6 @@ let
   cp = pkgs.callPackage;
 in
 {
-  awesome-lint = cp ./all/awesome-lint { };
   crossover = cp ./all/crossover.nix { };
   developercolorpicker = cp ./darwin/developercolorpicker.nix { };
   doctoc = cp ./all/doctoc.nix { };

b5314c06a25d7aa58fa35c47654fb22b86c600c1
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-22 23:24:38 -0700

    pkgs/crossover: make optional for other systems

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,9 +4,10 @@ let
 in
 {
   awesome-lint = cp ./all/awesome-lint { };
-  crossover = cp ./all/crossover.nix { };
   doctoc = cp ./all/doctoc.nix { };
   ultisnips-vscode = cp ./all/ultisnips-vscode.nix { };
-} // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux { } // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
+} // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+  crossover = cp ./linux/crossover.nix { };
+} // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
   developercolorpicker = cp ./darwin/developercolorpicker.nix { };
 }

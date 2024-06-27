deda498ff15218807915835493cca54f6c9d14cc
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-27 10:04:48 -0700

    home/xdg: set feed reader

--- home/linux/xdg.nix
+++ home/linux/xdg.nix
@@ -1,4 +1,5 @@
 # NOTE: DO NOT INCLUDE XDG.CONFIGFILE SETTINGS HERE :)
+{ pkgs, ... }:
 {
   xdg = {
     userDirs = {
@@ -9,6 +10,10 @@
       defaultApplications = {
         "x-scheme-handler/https" = "/etc/profiles/per-user/me/share/applications/librewolf.desktop";
         "x-scheme-handler/http" = "/etc/profiles/per-user/me/share/applications/librewolf.desktop";
+        "application/rss+xml" = "${pkgs.liferea}/share/applications/net.sourceforge.liferea.desktop";
+        "application/atom+xml" = "${pkgs.liferea}/share/applications/net.sourceforge.liferea.desktop";
+        "application/rdf+xml" = "${pkgs.liferea}/share/applications/net.sourceforge.liferea.desktop";
+        "x-scheme-handler/feed" = "${pkgs.liferea}/share/applications/net.sourceforge.liferea.desktop";
       };
     };
     configFile."user-dirs.locale".text = "en_US";

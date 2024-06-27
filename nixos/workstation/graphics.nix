ccc6e2ad11a2273296c79db1c664a5412c4f4a7e
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-27 10:24:18 -0700

    nixos/graphics: nixos.wiki -> wiki.nixos.org

--- nixos/workstation/graphics.nix
+++ nixos/workstation/graphics.nix
@@ -5,7 +5,7 @@
     # > On 64-bit systems, whether to support Direct Rendering for 32-bit applications (such as Wine).
     # > -- Manual
     # I enabled this to allow running the Epic Games Store through Lutris and Wine.
-    # See https://nixos.wiki/wiki/Lutris for more information.
+    # See https://wiki.nixos.org/wiki/Lutris for more information.
     enable32Bit = true; # Did you read the comment before turning this off?
   };
 }

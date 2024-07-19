1ac655dcd4906a146d2d9c9dbde30207a1259429
Tomodachi94 <tomodachi94@protonmail.com>
2024-07-18 18:15:34 -0700

    nixos/ime: migrate to new .enable and .type format

--- nixos/workstation/ime.nix
+++ nixos/workstation/ime.nix
@@ -2,7 +2,8 @@
 {
   # Setup Japanese keyboard
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk

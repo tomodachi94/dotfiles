25b098924dfce3c9259590e71563c219778ed0b4
Tomodachi94 <tomodachi94@protonmail.com>
2024-07-21 22:05:40 -0700

    pkgs/hp-flash: init at 3.24
    Useless without the kernel module.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,6 +10,7 @@ in
   zsh-window-title = cp ./all/zsh-window-title.nix { };
 } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
   crossover = cp ./linux/crossover.nix { };
+  hp-flash = cp ./linux/hp-flash.nix { };
 } // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
   developercolorpicker = cp ./darwin/developercolorpicker.nix { };
 }
---
+++ pkgs/linux/hp-flash.nix
@@ -0,0 +1,42 @@
+{ stdenvNoCC, fetchzip, autoPatchelfHook, openssl, kmod, lib }:
+stdenvNoCC.mkDerivation rec {
+  pname = "hp-flash";
+  version = "3.24";
+  src = fetchzip {
+    url = "https://ftp.hp.com/pub/softpaq/sp150501-151000/sp150953.tgz";
+    hash = "sha256-ofzqu5Y2g+QU0myJ4SF39ZJGXH1zXzX1Ys2FhXVTQKE=";
+    stripRoot = false;
+  };
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+  buildInputs = [
+    openssl
+    kmod
+  ];
+  installPhase = ''
+    tar -xzf non-rpms/hp-flash-${version}_x86_64.tgz
+    cd hp-flash-${version}_x86_64
+    # Nix builds do not run as root
+    # Use Nix paths for the output
+    # Use the POSIXy directory for documentation
+    # Script attempts to find libcrypto in non-Nix paths, which should never be true
+    substituteInPlace ./install.sh \
+      --replace-fail "exit" "true" \
+      --replace-fail "/opt/hp/hp-flash" "$out" \
+      --replace-fail "$out/docs" "$out/share/doc" \
+      --replace-fail "$out/bin/hp-" "$out/bin/.hp-" \
+      --replace-fail ". ./builds/test-distro.sh" "true"
+
+    substituteInPlace ./hp-flash ./hp-repsetup \
+      --replace-fail "/opt/hp/hp-flash" "$out" \
+      --replace-fail "/sbin/modprobe" "${kmod}/bin/modprobe" \
+      --replace-fail "/sbin/rmmod" "${kmod}/bin/rmmod" \
+      --replace-fail "$out/bin/hp-" "$out/bin/.hp-"
+
+    mv builds/hp-flash.u2204 bin/hp-flash
+    mv builds/hp-repsetup.u2204 bin/hp-repsetup
+    ./install.sh
+    mv $out/hp-{flash,repsetup} $out/bin
+  '';
+}

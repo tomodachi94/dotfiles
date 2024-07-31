0461f5654f2dc79f04f4ca6afeb10a0e46b02ecb
Tomodachi94 <tomodachi94@protonmail.com>
2024-07-31 14:09:01 -0700

    home/librewolf: manage extensions with module

---
+++ home/common/librewolf/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, tomolib, firefox-addons, config, ... }:
+let
+  firejailWrappers = tomolib.firejailWrappers { inherit pkgs; };
+in
+{
+  programs.librewolf = {
+    enable = true;
+    # package = firejailWrappers.librewolf;
+    settings = {
+      "webgl.disabled" = false;
+    };
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+      extensions = with firefox-addons.packages.${pkgs.system}; [
+        bitwarden
+        multi-account-containers
+        auto-tab-discard
+        ublacklist
+        indie-wiki-buddy
+      ];
+    };
+  };
+
+  home.sessionVariables = {
+    BROWSER = "${config.programs.librewolf.finalPackage}/bin/librewolf";
+  };
+}

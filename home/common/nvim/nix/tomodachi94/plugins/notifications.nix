d2104b7f7cf647ad09bd0b5ad78ddf5a765b3af3
Tomodachi94 <tomodachi94@protonmail.com>
2024-12-08 17:25:11 -0800

    home/nvim: fix

--- home/common/nvim/nix/tomodachi94/plugins/notifications.nix
+++ home/common/nvim/nix/tomodachi94/plugins/notifications.nix
@@ -1,6 +1,6 @@
 { pkgs, config, lib, ... }:
 {
-  home.packages = lib.optionals (config.local.eagerSetup.enableGraphicalApps && lib.mkIf config.local.eagerSetup.enableHeavyDevelop) [
+  home.packages = lib.optionals (config.local.eagerSetup.enableGraphicalApps && config.local.eagerSetup.enableHeavyDevelop) [
     pkgs.libnotify
   ];
 }

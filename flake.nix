4d5e5288d5d7309a606b3fdbada8d1c91e4d43d2
Tomodachi94 <tomodachi94@protonmail.com>
2026-08-24 18:23:07 -0700

    flake.nix: rename stylix

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };

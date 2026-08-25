bbb88a8febe7642f928bfe59dcfb6fa186a651fb
Tomodachi94 <tomodachi94@protonmail.com>
2026-08-24 19:18:19 -0700

    home/desktop-standalone-packages: add naps2

--- home/linux/desktop-standalone-packages.nix
+++ home/linux/desktop-standalone-packages.nix
@@ -6,6 +6,7 @@ in
   home.packages = lib.optionals (config.local.system.linux && config.local.eagerSetup.enableGraphicalApps) [
     pkgs.lxqt.qps
     pkgs.zotero
+    pkgs.naps2
     # tomopkgs.${pkgs.system}.crosshair-dot
   ];
 }

6a4fa15c5dd6fdc4f7fdfecca81294abd04c68c4
Tomodachi94 <tomodachi94@protonmail.com>
2026-08-25 22:31:37 -0700

    flake.lock: bump

--- flake.nix
+++ flake.nix
@@ -65,7 +65,6 @@
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
 
     dash2 = {

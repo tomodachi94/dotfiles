614eaf3beab82e6f898cfeed06a92fc300770edd
Tomodachi94 <tomodachi94@protonmail.com>
2026-08-26 13:35:43 -0700

    flake.lock: update

--- pkgs/linux/ircam-viewer.nix
+++ pkgs/linux/ircam-viewer.nix
@@ -1,13 +1,13 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  nix-update-script,
-  pkg-config,
-  installShellFiles,
-  SDL2,
-  SDL2_ttf,
-  ffmpeg,
+{ lib
+, stdenv
+, fetchFromGitHub
+, nix-update-script
+, pkg-config
+, installShellFiles
+, SDL2
+, SDL2_ttf
+, ffmpeg
+,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
